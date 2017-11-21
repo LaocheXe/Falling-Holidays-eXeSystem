5ce9692cb5c5bfe45a591616a2b5f97d79ad21a1
Travis Thoene <forbiddenchaos@gmail.com>
2017-11-21 16:21:56 -0600

    Fix: Correct Settings
    Requites Menus to be enable for Xmas Lights to work
    
    Known Issues: Normal lights don't display

--- fallingholidays/XMasLights_menu.php
+++ fallingholidays/XMasLights_menu.php
@@ -2,10 +2,27 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = " <div id='lights'>
-  <!-- lights go here -->
- </div>";
-echo $text;
+$pluginpref = e107::pref('fallingholidays');
+
+
+	if($pluginpref['xmasLightsType'] == 'breakable')
+	{
+		$text = " <div id='lights'>
+		<!-- lights go here -->
+		</div>";
+		
+		//echo $text;
+	}
+	elseif($pluginpref['xmasLightsType'] == 'normal')
+	{
+		$text = '<div id="christmas-lights">
+		<!-- lights go here -->
+		</div>';
+		
+		//echo $text;
+	}
+	
+	echo $text;
 
  
 ?>
