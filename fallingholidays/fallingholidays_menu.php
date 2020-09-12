f477fe19a562e2625b0df11265ffd0ac61d06e0c
Travis Thoene <LaocheXe@users.noreply.github.com>
2020-09-12 15:45:15 -0500

    Update fallingholidays_menu.php

--- fallingholidays/fallingholidays_menu.php
+++ fallingholidays/fallingholidays_menu.php
@@ -2,7 +2,7 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = " <div id='lights'>
+$text .= " <div id='lights'>
   <!-- lights go here -->
  </div>";
 $text .= '<div id="Div1" class="snow"></div>';
